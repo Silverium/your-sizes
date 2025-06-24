1d130ff795855b4e7d141801cfb1057cc4984ee1
Soldeplata <soldeplatadeveloper@gmail.com>
2025-06-24 18:36:34 +0200

    Refactor Collapsible component to use useBoolean hook for state management

--- components/Collapsible.tsx
+++ components/Collapsible.tsx
@@ -6,16 +6,17 @@ import { ThemedView } from '@/components/ThemedView';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { useBoolean } from 'usehooks-ts';
 
 export function Collapsible({ children, title }: PropsWithChildren & { title: string }) {
-  const [isOpen, setIsOpen] = useState(false);
+  const {value:isOpen, toggle: toggleIsOpen} = useBoolean(false);
   const theme = useColorScheme() ?? 'light';
 
   return (
     <ThemedView>
       <TouchableOpacity
         style={styles.heading}
-        onPress={() => setIsOpen((value) => !value)}
+        onPress={toggleIsOpen}
         activeOpacity={0.8}>
         <IconSymbol
           name="chevron.right"
